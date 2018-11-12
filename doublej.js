df92e9139e036f41ce841ecba51b0cfc581e1355
sbryce1031 <44955486+sbryce1031@users.noreply.github.com>
2018-11-11 20:16:34 -0500

    Add files via upload
    initial commit

---
+++ doublej.js
@@ -0,0 +1,69 @@
+var doublequestions = 
+[
+	{
+		"name": "More Acronyms",
+		"questions":
+		[
+			"RAM",
+			"ROM",
+			"RAID",
+			"GUI",
+			"HDMI"
+		]
+	},
+	{
+		"name": "Connection Ports",
+		"questions":
+		[
+			"This relatively new type of port can transfer data at a \"thunderously\" fast 40 Gbps",
+			"This type of port is used for networking computers and connecting to the internet",
+			"This type of port transfers high definition video and audio",
+			"This type of port is the most widely used today, and can \"bus\" data at 10 Gbps",
+			{"dailyd": true, "q": "If a port transfers data at 10 Gbps, it can transfer this many bits per second"}
+		]
+	},
+	{
+		"name": "Binary to Decimal",
+		"questions":
+		[
+			"10",
+			"11",
+			"100",
+			"111",
+			"101"
+		]
+	},
+	{
+		"name": "CPU",
+		"questions":
+		[
+			"This law predicts that the processing power of CPUs will double every two years",
+			"The two main parts of a CPU are the ALU and this",
+			"The four steps in the execution cycle are fetch, decode, execute, and this",
+			{"dailyd": true, "q": "This part of a CPU is made from a quartz crystal"},
+			"Doing this to your CPU will void its warranty, decrease its lifespan, and make it go faster"
+		]
+	},
+	{
+		"name": "Bits and Bytes",
+		"questions":
+		[
+			"There are this many bits in a byte",
+			"There are this many bytes in a gigabyte",
+			"There are this many bytes in a megabyte",
+			"There are this many bits in a nibble",
+			"There are this many bits in a kilobyte"
+		]
+	},
+	{
+		"name": "Storing Data",
+		"questions":
+		[
+			"Programs and data currently in use are primarily stored in this volatile memory (your lab computers have 2 GB of it)",
+			"SSD stands for this, a drive that is faster, less fragile, but more expensive than a mechanical hard drive",
+			"The thin metal discs in mechanical hard drives are called this",
+			"This type of RAID is fast, but will lose all your data if either disk dies",
+			"This type of RAID uses parity to be both fast and redundant"
+		]
+	}
+]
---
+++ questions.js
@@ -0,0 +1,70 @@
+var questions = 
+[
+	{
+		"name": "Acronyms",
+		"questions":
+		[
+			"CPU",
+			"USB",
+			"BIOS",
+			"ALU",
+			"OLED"
+		]
+	},
+	{
+		"name": "Early Computer History",
+		"questions":
+		[
+			"Who invented the difference engine?",
+			"Hollerith's Tabulating Machine was used to calculate this",
+			"Punch cards were first used in this type of machine",
+			"This poet was Ada Lovelace's father",
+			"Alan Turing died from eating this",
+		]
+	},
+	{
+		"name": "Personal Computer History",
+		"questions":
+		[
+			"Bill Gates founded this company",
+			"This company's PC was Time's Man of the Year in 1981",
+			"This is considered the first portable PC",
+			{"dailyd": true,
+			"q": "This company made the 8800, widely considered the first PC"},
+			"Steve Jobs and this other Steve founded Apple"
+		]
+	},
+	{
+		"name": "Potent Portable Computers",
+		"questions":
+		[
+			"An iPad is this type of computer",
+			"This type of laptop is ultra thin",
+			"Phones that aren't smartphones are called this",
+			"This cheap type of laptop runs an operating system made by Google",
+			"This type of laptop typically has a touchscreen paired with a detachable keyboard"
+		]
+	},
+	{
+		"name": "Peripherals",
+		"questions":
+		[
+			"Most keyboards use this layout, named for the first six keys",
+			"The tiny dots of color that make up a monitor are called this",
+			"The ratio of width-to-height in a montior is called this, for example 16:9",
+			"This keyboard layout is faster than QWERTY, but much less popular",
+			"A device that converts energy from one form to another is called this.  For example a microphone that converts sound waves to electrical signal"
+		]
+	},
+	{
+		"name": "Printers",
+		"questions":
+		[
+			"This type of printer sprays tiny dots of ink",
+			"This type of printer uses lasers and static electricity to fuse ink to pages",
+			"The C in CMYK stands for this color",
+			"This type of printer uses polymers instead of ink",
+			"The K in CMYK stands for this color"
+		]
+	}
+]
